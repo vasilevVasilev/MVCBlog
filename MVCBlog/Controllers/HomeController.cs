184dd4af030c1250e4499bf92843ab1685a0f00e
desktop <desktop@DESK>
2016-07-30 21:12:00 +0300

    Add project files.

---
+++ MVCBlog/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MVCBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace MVCBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var posts = db.Posts.Include(p => p.Author)
+                .OrderByDescending(p => p.Date).Take(3).ToList();
+
+            return View(posts);
+        }
+
+       
+    }
+}
---
+++ MVCBlog/Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Utils
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLength)
+        {
+            if (text == null || text.Length <= maxLength)
+                return text;
+            return text.Substring(0, maxLength) + " ...";
+        }
+    }
+}
